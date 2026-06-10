c31124111831de8f2bb7c38ac9aa6e4e9dd53c9f
Oleksii <alexasr00798@gmail.com>
2026-06-10 13:07:13 +0300

    v.3

--- app/@modal/(.)notes/[id]/NotePreview.tsx
+++ app/@modal/(.)notes/[id]/NotePreview.tsx
@@ -4,7 +4,7 @@ import {
   QueryClient,
 } from '@tanstack/react-query'
 import { fetchNoteById } from '@/lib/api'
-import NotePreviewClient from '../../../notes/filter/[...slug]/NotePreview.client'
+import NotePreviewClient from './NotePreview.client'
 export default async function NoteDetailsPage({
   params,
 }: {
